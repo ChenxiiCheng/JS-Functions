2b4511c2ceb6581bcbaf5d6bd43d5b7b017abbd9
ChenxiiCheng <chenxic1011@gmail.com>
2021-07-05 21:45:19 -0400

    feat: add es5 deep clone func

---
+++ src/deepClone.js
@@ -0,0 +1,22 @@
+// Object.prototype.toString({})  => '[object Object]'
+// Object.prototype.toString([])  => '[object Array]'
+
+function deepClone(origin, target) {
+  const tar = target || {};
+  const toStr = Object.prototype.toString;
+  const arrType = '[object Array]';
+
+  for (const key in origin) {
+    // 防止拿到原型上的key
+    if (origin.hasOwnProperty(key)) {
+      if (typeof origin[key] === 'object' && origin[key] !== null) {
+        tar[key] = toStr.call(origin[key]) === arrType ? [] : {};
+        deepClone(origin[key], tar[key]);
+      } else {
+        tar[key] = origin[key];
+      }
+    }
+  }
+
+  return tar;
+}
